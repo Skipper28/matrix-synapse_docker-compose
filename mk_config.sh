5dcda21787ff2d1b866765839b6a7da9746e6662
Skipper28 <ole@heidkamp.eu>
2022-02-07 19:47:44 +0000

    modified:   docker-compose.yaml 	modified:   mk_config.sh

--- mk_config.sh
+++ mk_config.sh
@@ -2,4 +2,4 @@ docker run -it --rm \
     --mount type=volume,src=./synapse/data/,dst=/data \
     -e SYNAPSE_SERVER_NAME=matrix.domain.eu \
     -e SYNAPSE_REPORT_STATS=yes \
-    matrixdotorg/synapse:latest generate
+        matrixdotorg/synapse:latest generate
