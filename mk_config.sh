20edefe652f6072e044a91fc63e88bf3247c85bd
Skipper28 <ole@heidkamp.eu>
2022-02-07 21:40:31 +0100

    Update mk_config.sh

--- mk_config.sh
+++ mk_config.sh
@@ -1,5 +1,5 @@
 docker run -it --rm \
     -v `pwd`"/synapse/data:/data" \
-    -e SYNAPSE_SERVER_NAME=matrix.heidkamp.eu \
+    -e SYNAPSE_SERVER_NAME=matrix.yourdomain.eu \
     -e SYNAPSE_REPORT_STATS=yes \
         matrixdotorg/synapse:latest generate
