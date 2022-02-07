c8f07e9fa0fb7692693e60b3761245e63b20a6ae
Skipper28 <ole@heidkamp.eu>
2022-02-07 19:19:50 +0000

    first commit

---
+++ mk_config.sh
@@ -0,0 +1,5 @@
+docker run -it --rm \
+    --mount type=volume,src=./synapse/data/,dst=/data \
+    -e SYNAPSE_SERVER_NAME=matrix.domain.eu \
+    -e SYNAPSE_REPORT_STATS=yes \
+    matrixdotorg/synapse:latest generate
